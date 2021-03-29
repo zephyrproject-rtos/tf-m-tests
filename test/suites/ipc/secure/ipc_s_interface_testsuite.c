64f41af74bf73bacb5e6ed02843f6dbee069daaf
Andreas Vibeto <andreas.vibeto@nordicsemi.no>
2021-03-29 06:26:05 -0700

    Tests: Consolidate test case naming
    Add s and ns tags to all test cases
    Consolidate test numbering, counting the s and ns suites in each group separately
    
    Signed-off-by: Andreas Vibeto <andreas.vibeto@nordicsemi.no>
    Change-Id: I7d58c8b87231dbf897145dc232e437522da66700

--- test/suites/ipc/secure/ipc_s_interface_testsuite.c
+++ test/suites/ipc/secure/ipc_s_interface_testsuite.c
@@ -17,7 +17,7 @@ static void tfm_ipc_test_1001(struct test_result_t *ret);
 
 static struct test_t ipc_veneers_tests[] = {
 #ifdef TFM_PARTITION_FFM11
-    {&tfm_ipc_test_1001, "TFM_IPC_TEST_1001",
+    {&tfm_ipc_test_1001, "TFM_S_IPC_TEST_1001",
      "Accessing stateless service from secure partition", {TEST_PASSED}},
 #endif
 };
@@ -28,7 +28,7 @@ void register_testsuite_s_ipc_interface(struct test_suite_t *p_test_suite)
 
     list_size = (sizeof(ipc_veneers_tests) / sizeof(ipc_veneers_tests[0]));
 
-    set_testsuite("IPC secure interface test (TFM_IPC_TEST_1XXX)",
+    set_testsuite("IPC secure interface test (TFM_S_IPC_TEST_1XXX)",
                   ipc_veneers_tests, list_size, p_test_suite);
 }
 
