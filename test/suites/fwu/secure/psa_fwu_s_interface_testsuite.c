b42f6609204f48180d19c3a9d5fe9d928b8887ae
Andreas Vibeto <andreas.vibeto@nordicsemi.no>
2021-04-28 09:13:46 +0200

    FWU: Fix wrong test case names
    Update test case names of secure FWU tests missed during the test case name changes
    
    Signed-off-by: Andreas Vibeto <andreas.vibeto@nordicsemi.no>
    Change-Id: Ib62b7682e56fc66f744986f9cd3c8e425885902f

--- test/suites/fwu/secure/psa_fwu_s_interface_testsuite.c
+++ test/suites/fwu/secure/psa_fwu_s_interface_testsuite.c
@@ -37,12 +37,12 @@ static struct test_t psa_fwu_s_tests[] = {
     {&tfm_fwu_test_common_010, "TFM_S_FWU_TEST_1010",
      "Query interface with NULL info", {TEST_PASSED} },
 #endif
-    {&tfm_fwu_test_common_011, "TFM_FWU_TEST_1011",
+    {&tfm_fwu_test_common_011, "TFM_S_FWU_TEST_1011",
      "Query interface invald image id", {TEST_PASSED} },
-    {&tfm_fwu_test_common_012, "TFM_FWU_TEST_1012",
+    {&tfm_fwu_test_common_012, "TFM_S_FWU_TEST_1012",
      "Accept interface", {TEST_PASSED} },
 #ifdef TFM_FWU_TEST_REQUEST_REBOOT
-    {&tfm_fwu_test_common_013, "TFM_FWU_TEST_1013",
+    {&tfm_fwu_test_common_013, "TFM_S_FWU_TEST_1013",
      "Reboot interface", {TEST_PASSED} },
 #endif
 };
