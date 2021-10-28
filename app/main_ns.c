a1499291ddd68d609a1dc1ac6bf6a1014905f621
Chris Brand <chris.brand@cypress.com>
2021-10-28 12:02:05 -0700

    Add end-of-line to log messages
    LOG_MSG() doesn't add \r\n, so they need to be part of
    the string.
    
    Change-Id: Ic8ad2fe8da15706798c156514b1330fc6f90f96f
    Signed-off-by: Chris Brand <chris.brand@cypress.com>

--- app/main_ns.c
+++ app/main_ns.c
@@ -82,10 +82,10 @@ static void tfm_ns_multi_core_boot(void)
 {
     int32_t ret;
 
-    LOG_MSG("Non-secure code running on non-secure core.");
+    LOG_MSG("Non-secure code running on non-secure core.\r\n");
 
     if (tfm_ns_wait_for_s_cpu_ready()) {
-        LOG_MSG("Error sync'ing with secure core.");
+        LOG_MSG("Error sync'ing with secure core.\r\n");
 
         /* Avoid undefined behavior after multi-core sync-up failed */
         for (;;) {
@@ -94,7 +94,7 @@ static void tfm_ns_multi_core_boot(void)
 
     ret = tfm_ns_mailbox_init(&ns_mailbox_queue);
     if (ret != MAILBOX_SUCCESS) {
-        LOG_MSG("Non-secure mailbox initialization failed.");
+        LOG_MSG("Non-secure mailbox initialization failed.\r\n");
 
         /* Avoid undefined behavior after NS mailbox initialization failed */
         for (;;) {
