d44a0bd940bc69353e867e65f150cd97283ffd18
David Hu <david.hu@arm.com>
2022-01-18 14:51:35 +0800

    Hotfix: Fix tests build issues
    - Remove unused test/suites folder
    - Re-enable TF-M to build secure tests alone by including raw log
      library in NS tests and BL2 tests.
    - Add TEST_BL2 flag to select raw log in BL2 tests instead of using
      DOMAIN_NS=1. Setting DOMAIN_NS=1 in BL2 build is confusing.
    - Remove root CMakeLists.txt as NS app, raw log and tests builds are
      split again.
    
    Signed-off-by: David Hu <david.hu@arm.com>
    Change-Id: I01e2e80d63bfebb54d056c3e03a346b466a5e025

--- test/framework/test_log.h
+++ test/framework/test_log.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021, Arm Limited. All rights reserved.
+ * Copyright (c) 2021-2022, Arm Limited. All rights reserved.
  *
  * SPDX-License-Identifier: BSD-3-Clause
  *
@@ -8,7 +8,7 @@
 #ifndef __TEST_LOG_H__
 #define __TEST_LOG_H__
 
-#if DOMAIN_NS == 1
+#if (DOMAIN_NS == 1) || defined(TEST_BL2)
 #include "tfm_log_raw.h"
 #else
 #include "tfm_sp_log.h"
@@ -18,7 +18,7 @@
 extern "C" {
 #endif
 
-#if DOMAIN_NS == 1
+#if (DOMAIN_NS == 1) || defined(TEST_BL2)
 #define TEST_LOG(...) tfm_log_printf(__VA_ARGS__)
 #else
 #define TEST_LOG(...) tfm_sp_log_printf(__VA_ARGS__)
