40d7d9a5619a52a0d027984dd33841d0c5b06af2
Kevin Peng <kevin.peng@arm.com>
2023-10-10 14:48:58 +0800

    Build: Refine psa_arch_tests configuration
    - Now command line options can be passed to SPE build.
    - The TARGET and CPU_ARCH can be deduced from TF-M configs.
    - Align with the new test config setup approach.
    - Other improvements and fixes.
    
    Change-Id: Ic29849526f75dc4395a5599374356342463b61db
    Signed-off-by: Kevin Peng <kevin.peng@arm.com>

--- tests_psa_arch/spe/config/config_test_psa_api.cmake
+++ tests_psa_arch/spe/config/config_test_psa_api.cmake
@@ -4,24 +4,13 @@
 # SPDX-License-Identifier: BSD-3-Clause
 #
 #-------------------------------------------------------------------------------
-#
-# This is a configuration files for building SPE, necessary for PSA API Arch tests.
-# It shall be passed to SPE build via TFM_EXTRA_CONFIG_PATH option.
-#-------------------------------------------------------------------------------
-
-# Do not forget to specify configuration file While building SPE via:
-# -DPROJECT_CONFIG_HEADER_FILE=<>/config_test_psa_api.h
-
-set(TEST_S                  OFF      CACHE BOOL      "Whether to build S regression tests")
-set(TFM_S_REG_TEST          OFF      CACHE BOOL      "Enable S regression test")
-set(NS                      OFF      CACHE BOOL      "Enalbe NS side build")
 
 if ("${TEST_PSA_API}" STREQUAL "IPC")
-    # PSA Arch test partitions only support IPC model so far
     set(CONFIG_TFM_SPM_BACKEND      "IPC"       CACHE STRING    "The SPM backend [IPC, SFN]")
-    set(TFM_PARTITION_FF_TEST   ON)
-else()
-    set(TFM_PARTITION_FF_TEST   OFF)
+
+    configure_file(${CMAKE_CURRENT_LIST_DIR}/../tfm_psa_ff_test_manifest_list.yaml
+                   ${CMAKE_BINARY_DIR}/tfm_psa_ff_test_manifest_list.yaml)
+    list(APPEND TFM_EXTRA_MANIFEST_LIST_FILES ${CMAKE_BINARY_DIR}/tfm_psa_ff_test_manifest_list.yaml)
 endif()
 
 if ("${TEST_PSA_API}" STREQUAL "INITIAL_ATTESTATION")
@@ -43,3 +32,10 @@ if ("${TEST_PSA_API}" STREQUAL "STORAGE")
     set(TFM_PARTITION_CRYPTO                   ON       CACHE BOOL      "Enable Crypto partition")
     set(TFM_PARTITION_PLATFORM                 ON       CACHE BOOL      "Enable Platform partition")
 endif()
+
+set(PROJECT_CONFIG_HEADER_FILE
+    ${CMAKE_CURRENT_LIST_DIR}/../config/config_test_psa_api.h
+    CACHE FILEPATH "The psa-arch-test config header file")
+
+# Set default value for INCLUDE_PANIC_TESTS explicitly
+set(INCLUDE_PANIC_TESTS     0   CACHE BOOL      "Include panic tests")
