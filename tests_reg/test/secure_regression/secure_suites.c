8c15c0474aa7cad88b403f050d6f32e2d073685b
Antonio de Angelis <Antonio.deAngelis@arm.com>
2024-10-15 11:46:25 +0100

    PS: Do not test PS_ROLLBACK_PROTECTION test suite if not built
    If the PS_ROLLBACK_PROTECTION feature is not enabled, then do
    not build and run the associated test suite.
    
    Signed-off-by: Antonio de Angelis <antonio.deangelis@arm.com>
    Change-Id: I29fb2bddf5e54be2f62d973236085bb77d060d54

--- tests_reg/test/secure_regression/secure_suites.c
+++ tests_reg/test/secure_regression/secure_suites.c
@@ -54,11 +54,12 @@ static struct test_suite_t test_suites[] = {
 #ifdef TEST_S_PS
     {&register_testsuite_s_psa_ps_interface, 0, 0, 0},
     {&register_testsuite_s_psa_ps_reliability, 0, 0, 0},
-
 #ifdef PS_TEST_NV_COUNTERS
+#if defined(PS_ROLLBACK_PROTECTION) && (PS_ROLLBACK_PROTECTION == 1)
     {&register_testsuite_s_rollback_protection, 0, 0, 0},
-#endif
-#endif
+#endif /* PS_ROLLBACK_PROTECTION == 1 */
+#endif /* PS_TEST_NV_COUNTERS */
+#endif /* TEST_S_PS */
 
 #ifdef TEST_S_ITS
     /* Secure ITS test cases */
