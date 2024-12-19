14ca4b7cbae3ad3a0319a7fe95ebefb301af965b
David Vincze <david.vincze@arm.com>
2024-12-19 19:31:57 +0000

    Build: Remove ATTEST_INCLUDE_TEST_CODE
    Remove ATTEST_INCLUDE_TEST_CODE option as it is now unused.
    
    Change-Id: I31863afc9095f6b1e4d37804f47198c37819ef0c
    Signed-off-by: David Vincze <david.vincze@arm.com>

--- tests_reg/test/secure_fw/suites/attestation/attest_token_test.h
+++ tests_reg/test/secure_fw/suites/attestation/attest_token_test.h
@@ -50,8 +50,7 @@ extern "C" {
  * are checked.
  *
  * The generated token will be decoded.
- * The tag in COSE_Mac0 structure will be verified in secure side when
- * INCLUDE_TEST_CODE is enabled.
+ * The tag in COSE_Mac0 structure will be verified in secure side.
  */
 int_fast16_t decode_test_symmetric_initial_attest(void);
 
